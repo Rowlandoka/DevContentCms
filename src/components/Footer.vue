1bd85ececb577a4b51f8cbf7378761c90d72d7d0
ROWLAND EDODI OKA <rowlandoka@gmail.com>
2020-12-14 03:00:12 +0100

    first Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+    <footer class="bg-gray-100 border-t-2 border-gray-300 py-10 text-sm">
+        <div class="max-w-5xl mx-auto pb-8 mb-6 border-b border-gray-300 px-4 lg:px-0">
+            <div class="flex flex-wrap items-start justify-sta">
+              <div class="lg:w-1/3 w-full lg:mb-0 mb-6">
+                 <h5 class="font-semibold text-blue-400 mb-2">DevStoreCms</h5>
+                 <ul class="leading-loose">
+                     <li><a href="#" class="text-gray-600 hover:underline font-semibold">Home</a></li>
+                     <li><a href="#" class="text-gray-600 hover:underline font-semibold">Blog</a></li>
+                     <li><a href="#" class="text-gray-600 hover:underline font-semibold">Articles</a></li>
+                     <li><a href="#" class="text-gray-600 hover:underline font-semibold">Faq</a></li>
+                     <li><a href="#" class="text-gray-600 hover:underline font-semibold">Contacts</a></li>
+                 </ul>
+              </div>
+              <div class="lg:w-1/3 w-full lg:mb-0 mb-6">
+                <h5 class="font-semibold text-blue-400 mb-2">Contacts</h5>
+                <ul class="leading-loose">
+                    <p class="font-semibold text-gray-700 pb-2">21 Revolution Street Calabar, Nigeria</p>
+                    <p class="font-semibold text-gray-700 pb-2">+234 706 25541</p>
+                    <p class="font-semibold text-gray-700 pb-2"> contact@devstorecms.com</p>
+                 </ul>
+              </div>
+              <div class="lg:w-1/3 w-full lg:mb-0 mb-6">
+               <h5 class="font-semibold text-blue-400 mb-2">About DevStoreCms</h5>
+               <p class="mb-2 lg:mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus odit debitis esse error eius optio voluptates quasi dolores veniam architecto aut nostrum dolor fugit, accusantium consequatur modi ullam asperiores? Quod?</p>
+                <div class="flex mt-3">
+                    <a href="#" class="mr-6 text-blue-500 font-semibold hover:text-gray-900">facebook</a>
+                    <a href="#" class="mr-6 text-blue-500 font-semibold hover:text-gray-900">Twitter</a>
+                    <a href="#" class="mr-6 text-blue-500 font-semibold hover:text-gray-900">Google+</a>
+                    <a href="#" class="mr-6 text-blue-500 font-semibold hover:text-gray-900">Instagram</a>
+                </div>
+              </div>
+        </div>
+        </div>
+        <div class="w-full px-8 py-2 lg:flex justify-between items-center">
+        <p class="mb-2 lg:mb-0 text-blue-700">© DevStoreCms 2020</p>
+        <div class="flex">
+            <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
+            <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
+        </div>
+    </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
